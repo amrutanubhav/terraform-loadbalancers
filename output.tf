3f66340a4e5d531b62d201606c14dade14d49e33
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-14 09:47:42 +0530

    updating

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
+output "PUBLIC_ALB_ARN" {
+    value = module.alb-public.ALB_ARN
+}
+
+output "PRIVATE_ALB_ARN" {
+    value = module.alb-private.ALB_ARN
+}
+
+
+
+
+
 # output "VPC_ID" {
 #     value = module.vpc.VPC_ID 
 # }
