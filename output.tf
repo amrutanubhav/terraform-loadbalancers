2ca0df3fc73d37e61e93d7a10d5522420693029f
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-23 23:20:46 +0530

    updating

--- output.tf
+++ output.tf
@@ -1,76 +1,20 @@
 output "PUBLIC_ALB_ARN" {
-    value = module.alb-public.ALB_ARN
+    value =  module.alb-public.ALB_ARN
 }
 
 output "PRIVATE_ALB_ARN" {
-    value = module.alb-private.ALB_ARN
+    value =  module.alb-private.ALB_ARN
 }
 
 output "PRIVATE_LISTENER_ARN" {
-    value = module.alb-private.LISTENER_ARN[0]
+    value  = module.alb-private.LISTENER_ARN[0]
 }
 
+
 output "PUBLIC_ALB_ADDRESS" {
-    value = module.alb-public.ALB_ADDRESS
+    value =  module.alb-public.ALB_ADDRESS
 }
 
 output "PRIVATE_ALB_ADDRESS" {
-    value = module.alb-private.ALB_ADDRESS
+    value =  module.alb-private.ALB_ADDRESS
 }
-
-
-
-
-
-
-
-
-
-
-# output "VPC_ID" {
-#     value = module.vpc.VPC_ID 
-# }
-
-# output "VPC_CIDR" {
-#     value = module.vpc.VPC_CIDR
-# }
-
-# output "PRIVATE_SUBNET_IDS" {
-#     value = module.vpc.PRIVATE_SUBNET_IDS
-# }
-
-# output "PUBLIC_SUBNET_IDS" {
-#     value = module.vpc.PUBLIC_SUBNET_IDS
-# }
-
-# output "PRIVATE_SUBNET_CIDR" {
-#     value = module.vpc.PRIVATE_SUBNET_CIDR
-# }
-
-# output "PUBLIC_SUBNET_CIDR" {
-#     value = module.vpc.PUBLIC_SUBNET_CIDR
-# }
-
-# output "DEFAULT_VPC_CIDR" {
-#     value = module.vpc.DEFAULT_VPC_CIDR
-# }
-
-# output "DEFAULT_VPC_ID" {
-#     value = module.vpc.DEFAULT_VPC_ID
-# }
-
-# output "PRIVATE_HOSTEDZONE_ID" {
-#     value =  var.PRIVATE_HOSTEDZONE_ID
-# }
-
-# output "PUBLIC_HOSTEDZONE_ID" {
-#     value = var.PUBLIC_HOSTEDZONE_ID
-# }
-
-# output "PRIVATE_HOSTEDZONE_NAME" {
-#     value = var.PRIVATE_HOSTEDZONE_NAME
-# }
-
-# output "PUBLIC_HOSTEDZONE_NAME" {
-#     value = var.PUBLIC_HOSTEDZONE_NAME
-# }
